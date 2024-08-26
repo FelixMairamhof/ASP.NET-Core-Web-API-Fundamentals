3c030e49261f0d3cd7740be5b925ed397f5cfca1
Felix Mairamhof <feberg.k@gmail.com>
2024-08-26 15:31:09 +0200

    File Api

---
+++ CityInfoAPI/Controllers/FilesController.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.StaticFiles;
+
+namespace CityInfoAPI.Controllers
+{
+    [Route("api/files")]
+    [ApiController]
+    public class FilesController : Controller
+    {
+        private readonly FileExtensionContentTypeProvider _fileExtensionContentTypeProvider;
+        public FilesController(FileExtensionContentTypeProvider fileExtensionContentTypeProvider)
+        {
+            _fileExtensionContentTypeProvider = fileExtensionContentTypeProvider
+                ?? throw new System.ArgumentNullException(nameof(fileExtensionContentTypeProvider));
+        }
+        [HttpGet("{fileId}")]
+        public ActionResult GetFile(string fileId)
+        {
+            var pathToFile = "Database ER diagram (crow's foot) - Page 1.svg";
+
+            if(!System.IO.File.Exists(pathToFile))
+            {
+                return NotFound();
+            }
+
+            var bytes = System.IO.File.ReadAllBytes(pathToFile);
+            return File(bytes, "text/plain", Path.GetFileName(pathToFile));
+        }
+    }
+}
